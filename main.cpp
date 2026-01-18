7ce6a2c4a25374ed2a7f8f3fe3adbc3e2f3c72a3
hatuna-827 <hatuna827yadaka@gmail.com>
2026-01-18 17:28:11 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@
 #include <string>
 std::regex Indent(R"(^(\s*)(.*)$)");
 std::regex Heading(R"(^\s*(#{1,6})\s*(.*)$)");
+std::regex HeadingLine1(R"(^\s*=+\s*$)");
+std::regex HeadingLine2(R"(^\s*-+\s*$)");
 std::regex Break(R"(^(.*)(\s\s|\\)$)");
-std::regex Bold_Italic(R"(([^\\]|^)([*_]{3})((?:(?!\2).)+)\2)");
-std::regex Bold(R"(([^\\]|^)([*_]{2})((?:(?!\2).)+)\2)");
+std::regex Bold_Italic(R"(([^\\]|^)([*_])\2\2((?:(?!\2\2\2).)+)\2\2\2)");
+std::regex Bold(R"(([^\\]|^)([*_])\2((?:(?!\2\2).)+)\2\2)");
 std::regex Italic(R"(([^\\]|^)([*_])((?:(?!\2).)+)\2)");
 std::regex Partition(R"(^\s*([-_*])(?:\s*\1){2,}\s*$)");
-std::regex Strikethrough(R"(~~((?!~~).+?)~~)");
+std::regex Strikethrough(R"((^|[^\\])~~((?!~~).+?[^\\])~~)");
 std::regex InlineCode(R"((^|[^\\`])(`+)([^`](?:(?!\2).)+?[^\\`])\2(?!`))");
 std::regex CodeBlock(R"(^ {0,3}(`{3,})[^`]*$)");
 std::regex CodeBlockClose(R"(^ {0,3}(`{3,})\s*$)");
@@ -18,9 +20,9 @@ std::regex IndentCodeBlock(R"(^(?: {4}|\t)(.*)$)");
 std::regex Image(R"(!\[(.*?)\]\(\s*(\S*?)(?:\s+\"(.*?)\")?\s*\))");
 std::regex Anchor(R"(\[(.*?)\]\(\s*(\S*?)(?:\s+\"(.*?)\")?\s*\))");
 std::regex Reference(R"(^\s*\[(.*)\]:\s*(\S*)\s+\"(.*)\"\s*$)");
-std::regex URL(R"(<(https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+)>)");
+std::regex URL(R"(&lt;(https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+)&gt;)");
 std::regex rawURL(R"((?:^|\s)(https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+)(?:$|\s))");
-std::regex Mail(R"(<([\w\d.+-]+@([a-zA-Z\d][a-zA-Z\d-]*[a-zA-Z\d]*\.)+[a-zA-Z]{2,})>)");
+std::regex Mail(R"(&lt;([\w\d.+-]+@([a-zA-Z\d][a-zA-Z\d-]*[a-zA-Z\d]*\.)+[a-zA-Z]{2,})&gt;)");
 std::regex rawMail(R"((?:^|\s)([\w\d.+-]+@([a-zA-Z\d][a-zA-Z\d-]*[a-zA-Z\d]*\.)+[a-zA-Z]{2,})(?:$|\s))");
 std::regex UnorderedList(R"(^(\s{0,3}[-+*]\s)(.*)$)");
 std::regex OrderedList(R"(^(\s{0,3}\d+\.\s)(.*)$)");
@@ -28,7 +30,32 @@ std::regex Blockquotes(R"(^(\s{0,3}>\s)(.*)$)");
 std::regex Checkbox(R"(^\[[ xX]\])");
 std::regex Table(R"(^(?:\s*\|\s*.*?)+\|\s*$)");
 std::regex TableMeta(R"(^(?:\s*\|\s*:?-+:?)+\|\s*$)");
+std::regex HtmlTagOpen(R"(\\?<)");
+std::regex HtmlTagClose(R"(\\?>)");
 std::regex Escape(R"(\\([\!\"\#\$\%\&\'\(\)\-\^\\\@\[\;\:\]\,\.\/\\\=\~\|\`\{\+\*\}\<\>\?\_]))");
+
+class process
+{
+public:
+  std::string md;
+  std::string html = "";
+  std::string header = "";
+  std::string footer = "";
+  process(std::string init_md) { md = init_md; }
+  void add_tag(std::string new_header, std::string new_footer)
+  {
+    header = header + new_header;
+    footer = new_footer + footer;
+  }
+  void add_header(std::string new_header)
+  {
+    header = new_header + header;
+  }
+  void add_footer(std::string new_footer)
+  {
+    footer = footer + new_footer;
+  }
+};
 
 int main(int argc, char *argv[])
 {
@@ -51,10 +78,16 @@ int main(int argc, char *argv[])
     std::cerr << "Error: The file could not be opened" << std::endl;
     return 1;
   }
-  oFile << "<!DOCTYPE HTML><head><meta charset=\"utf-8\"/>";
+  oFile << "<!DOCTYPE html><html><head><meta charset=\"utf-8\"/>";
   if (!cssFileName.empty())
     oFile << "<link rel=\"stylesheet\" href = \"" << cssFileName << "\">";
   oFile << "<title>" << outFileName << "</title></head><body>";
+  std::vector<process> p;
+  std::string buf;
+  while (std::getline(iFile, buf))
+  {
+    p.emplace_back(buf);
+  }
   bool br_flag = false;
   bool p_flag = false;
   bool p_close_flag = false;
@@ -66,171 +99,155 @@ int main(int argc, char *argv[])
   int blqu_cnt = 0;
   std::vector<int> list_cnt = {0};
   std::vector<char> indent_type = {' '};
-  std::string line_md;
-  std::string line_html;
-  std::string line_html_start;
-  std::string line_html_content;
-  std::string line_html_end;
   std::smatch m;
-  while (std::getline(iFile, line_md))
+  for (int i = 0; i < p.size(); ++i)
   {
-    std::regex_match(line_md, m, Indent);
+    // Outside the fence code
+    std::regex_match(p[i].md, m, Indent);
     indent = m[1].str().length() - list_cnt.back();
     if (indent < 0)
     {
       if (p_flag)
-        oFile << "</p>";
+        p[i - 1].add_footer("</p>");
       p_flag = false;
       if (code_cnt == 0)
       {
         if (code_flag)
-          oFile << "</pre>";
+          p[i - 1].add_footer("</pre>");
       }
       else
       {
         if (code_flag)
-          oFile << "</code>";
-        oFile << "</pre>";
+          p[i - 1].add_footer("</code>");
+        p[i - 1].add_footer("</pre>");
       }
       code_flag = false;
       code_cnt = 0;
       while (indent < 0)
       {
         if (indent_type.back() == '1')
-          oFile << "</ol>";
+          p[i - 1].add_footer("</ol>");
         else
-          oFile << "</ul>";
+          p[i - 1].add_footer("</ul>");
         list_cnt.pop_back();
         indent_type.pop_back();
         indent = m[1].str().length() - list_cnt.back();
       }
     }
-    line_md = std::string(indent, ' ') + m[2].str();
-    line_html = "";
+    p[i].md = std::string(indent, ' ') + m[2].str();
+    p[i].md = std::regex_replace(p[i].md, HtmlTagOpen, "&lt;");
+    p[i].md = std::regex_replace(p[i].md, HtmlTagClose, "&gt;");
     if (code_cnt == 0)
     {
-      // Outside the fence code
       p_close_flag = p_flag;
       p_flag = false;
       code_close_flag = code_flag;
-      if (line_md.empty())
+      if (p[i].md.empty())
       {
       }
-      else if (std::regex_match(line_md, m, IndentCodeBlock))
+      else if (std::regex_match(p[i].md, m, IndentCodeBlock))
       {
         if (!code_flag)
-          line_html += "<pre>";
-        line_html += (m[1].str() + "\n");
+          p[i].add_tag("<pre>", "");
+        p[i].html = m[1].str() + "\n";
         code_flag = true;
         code_close_flag = false;
       }
-      else if (std::regex_match(line_md, m, CodeBlock))
+      else if (std::regex_match(p[i].md, m, CodeBlock))
       {
-        line_html += "<pre>";
+        p[i].add_tag("<pre>", "");
         code_cnt = m[1].str().length();
         if (m[2].str().empty())
         {
-          line_html += "<code>";
+          p[i].add_tag("<code>", "");
           code_flag = true;
         }
       }
-      else if (std::regex_match(line_md, m, Partition))
+      // else if (std::regex_match(p[i].md, m, HeadingLine1))
+      // {
+      //   p[i - 1].add_tag("<h1>", "</h1>");
+      // }
+      // else if (std::regex_match(p[i].md, m, HeadingLine2))
+      // {
+      //   p[i - 1].add_tag("<h2>", "</h2>");
+      // }
+      else if (std::regex_match(p[i].md, m, Partition))
       {
-        line_html += "<hr>";
+        p[i].html = "<hr>";
       }
       else
       {
-        line_html_start = "";
-        line_html_end = "";
-        list_blqu_flag = true;
-        // while (list_blqu_flag)
-        // {
-        //   list_blqu_flag = false;
-        //   // list
-        //   while (std::regex_match(line_md, m, UnorderedList))
-        //   {
-        //     list_blqu_flag = true;
-        //   }
-        //   while (std::regex_match(line_md, m, OrderedList))
-        //   {
-        //     list_blqu_flag = true;
-        //   }
-        //   // blqu
-        //   while (std::regex_match(line_md, m, Blockquotes))
-        //   {
-        //     list_blqu_flag = true;
-        //   }
-        // }
-        if (std::regex_match(line_md, m, Heading))
+        if (std::regex_match(p[i].md, m, Heading))
         {
-          line_html_start = (line_html_start + "<h" + std::to_string(m[1].str().length()) + ">");
-          line_html_content = m[2].str();
-          line_html_end = ("</h" + std::to_string(m[1].str().length()) + ">" + line_html_end);
+          p[i].add_tag("<h" + std::to_string(m[1].str().length()) + ">", "</h" + std::to_string(m[1].str().length()) + ">");
+          p[i].html = m[2].str();
         }
         else
         {
-          line_html_content = line_md;
+          p[i].html = p[i].md;
           p_flag = true;
           if (p_close_flag)
           {
             p_close_flag = false;
             if (!br_flag)
-              line_html_start += " ";
+              p[i].add_tag(" ", "");
           }
           else
-            line_html_start += "<p>";
-          if (std::regex_match(line_md, m, Break))
+            p[i].add_tag("<p>", "");
+          if (std::regex_match(p[i].md, m, Break))
           {
-            line_html_content = std::regex_replace(line_html_content, Break, "$1");
-            line_html_end = ("<br>" + line_html_end);
+            p[i].html = std::regex_replace(p[i].html, Break, "$1");
+            p[i].add_tag("", "<br>");
             br_flag = true;
           }
           else
             br_flag = false;
         }
-        line_html_content = std::regex_replace(line_html_content, Bold_Italic, "$1<strong><em>$3</em></strong>");
-        line_html_content = std::regex_replace(line_html_content, Bold, "$1<strong>$3</strong>");
-        line_html_content = std::regex_replace(line_html_content, Italic, "$1<em>$3</em>");
-        line_html_content = std::regex_replace(line_html_content, Strikethrough, "<s>$1</s>");
-        line_html_content = std::regex_replace(line_html_content, InlineCode, "$1<code>$3</code>");
-        line_html_content = std::regex_replace(line_html_content, Image, "<img alt=\"$1\" src=\"$2\" title=\"$3\">");
-        line_html_content = std::regex_replace(line_html_content, Anchor, "<a href=\"$2\" title=\"$3\">$1</a>");
-        line_html_content = std::regex_replace(line_html_content, URL, "<a href=\"$1\">$1</a>");
-        line_html_content = std::regex_replace(line_html_content, rawURL, "<a href=\"$1\">$1</a>");
-        line_html_content = std::regex_replace(line_html_content, Mail, "<a href=\"mailto:$1\">$1</a>");
-        line_html_content = std::regex_replace(line_html_content, rawMail, "<a href=\"mailto:$1\">$1</a>");
-        line_html = line_html_start + line_html_content + line_html_end;
+        p[i].html = std::regex_replace(p[i].html, Bold_Italic, "$1<strong><em>$3</em></strong>");
+        p[i].html = std::regex_replace(p[i].html, Bold, "$1<strong>$3</strong>");
+        p[i].html = std::regex_replace(p[i].html, Italic, "$1<em>$3</em>");
+        p[i].html = std::regex_replace(p[i].html, Strikethrough, "$1<s>$2</s>");
+        p[i].html = std::regex_replace(p[i].html, InlineCode, "$1<code>$3</code>");
+        p[i].html = std::regex_replace(p[i].html, Image, "<img alt=\"$1\" src=\"$2\" title=\"$3\">");
+        p[i].html = std::regex_replace(p[i].html, Anchor, "<a href=\"$2\" title=\"$3\">$1</a>");
+        p[i].html = std::regex_replace(p[i].html, URL, "<a href=\"$1\">$1</a>");
+        p[i].html = std::regex_replace(p[i].html, rawURL, "<a href=\"$1\">$1</a>");
+        p[i].html = std::regex_replace(p[i].html, Mail, "<a href=\"mailto:$1\">$1</a>");
+        p[i].html = std::regex_replace(p[i].html, rawMail, "<a href=\"mailto:$1\">$1</a>");
+        p[i].html = std::regex_replace(p[i].html, Escape, "$1");
       }
       if (code_close_flag)
       {
-        line_html = ("</pre>" + line_html);
+        p[i - 1].add_footer("</pre>");
         if (code_cnt == 0)
           code_flag = false;
       }
       if (p_close_flag)
-        line_html = ("</p>" + line_html);
-      line_html = std::regex_replace(line_html, Escape, "$1");
+        p[i - 1].add_footer("</p>");
     }
     else
     {
       // Inside the fence code
-      std::regex_match(line_md, m, CodeBlockClose);
+      std::regex_match(p[i].md, m, CodeBlockClose);
       if (m[1].str().length() == code_cnt)
       {
+        p[i].add_tag("", "</pre>");
         if (code_flag)
-          line_html += "</code>";
-        line_html += "</pre>";
+          p[i].add_tag("", "</code>");
         code_cnt = 0;
         code_flag = false;
       }
       else
       {
-        line_html = line_md + "\n";
+        p[i].html = p[i].md + "\n";
       }
     }
-    oFile << line_html;
   }
-  oFile << "</body></HTML>";
+  for (int i = 0; i < p.size(); ++i)
+  {
+    oFile << p[i].header << p[i].html << p[i].footer;
+  }
+  oFile << "</body></html>";
   oFile.close();
   iFile.close();
   return 0;
